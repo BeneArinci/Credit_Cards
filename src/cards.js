0a36c76877be4e9aed59cb68ec91b6d158ce56b3
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-09-29 17:51:45 +0100

    Added object with all available cards and their info

---
+++ src/cards.js
@@ -0,0 +1,26 @@
+export const cards = [
+  {
+    name: "Student Life",
+    apr: "18.9%",
+    balance_transfer_offer_duration: "0 Months",
+    purchase_offer_duration: "6 Months",
+    credit_available: 1200,
+    availability: "students"
+  },
+  {
+    name: "Anywhere Card",
+    apr: "33.9%",
+    balance_transfer_offer_duration: "0 Months",
+    purchase_offer_duration: "0 Months",
+    credit_available: 300,
+    availability: "anyone"
+  },
+  {
+    name: "Liquid Card",
+    apr: "33.9%",
+    balance_transfer_offer_duration: "12 Months",
+    purchase_offer_duration: "6 Months",
+    credit_available: 3000,
+    availability: "minimum income"
+  }
+]
