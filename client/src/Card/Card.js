a88efe311c99206535805c24744e5314e01c6dae
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-10-03 11:50:44 +0100

    Adding and removing card credit based on selection

--- client/src/Card/Card.js
+++ client/src/Card/Card.js
@@ -3,14 +3,33 @@ import React from "react";
 import "./Card.css"
 
 class Card extends React.Component {
+  constructor(props){
+    super()
+    this.state = {
+      selected: false
+    }
+    this.selectDeselectCard = this.selectDeselectCard.bind(this)
+  }
 
- // ({cardinfo, addCredit, removeCredit})
+  selectDeselectCard() {
+    const {cardinfo} = this.props
+    if (this.state.selected === false) {
+      this.props.addCredit(cardinfo.credit_available)
+      this.setState({selected: true})
+      console.log("selected")
+    } else {
+      this.props.removeCredit(cardinfo.credit_available)
+      this.setState({selected: false})
+      console.log("deselected")
+    }
+
+  }
+ 
   render(){
     const {cardinfo, addCredit, removeCredit} = this.props
     return (
       <article className="card shadow-3 grow pointer center mw5 mw6-ns br3 hidden ba b--black-10 mv4"
-      onClick={() => addCredit(cardinfo.credit_available)}
-      onClick={() => removeCredit(cardinfo.credit_available)}>
+      onClick={() => this.selectDeselectCard()}>
         <h1 className="f3 bg-near-white br3 br--top orange mv0 pv2 ph3">{cardinfo.name}</h1>
         <div className="info-list bg-washed-green pa3 br3 b--black-10">
           <div className="f6 f4-ns lh-copy measure">
