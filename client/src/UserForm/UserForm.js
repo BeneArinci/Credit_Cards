074edebe4f313088c98c48985fb278518959dd10
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-10-04 17:53:39 +0100

    Form styling

--- client/src/UserForm/UserForm.js
+++ client/src/UserForm/UserForm.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './UserForm.css'
 
 const UserForm = ({onNameChange, onSubmit, onEmploymentChange, onIncomeChange}) => {
   return(
-    <article className="br2 ba dark-gray b--black-10 shadow-2 mv4 w-100 w-50-m w-25-l mw5 center" data-testid = "user-form">
+    <article className="form br2 ba dark-gray b--black-10 bg-washed-green shadow-2 w-100 w-50-m w-25-l mw5 center" data-testid = "user-form">
     <main className="pa4 black-80">
       <div className="measure center">
         <fieldset className="ba b--transparent ph0 mh0">
-          <legend className="f4 fw6 ph0 mh0">Insert your data for checking your credit availability</legend>
+          <legend className="f4 fw6 ph0 mh0">Welcome! 👋 <br />Insert your data for checking your credit availability</legend>
           <div className="mt3">
             <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
             <input className="pa2 input-reset ba bg-transparent hover-bg-light-green hover-black w-100" type="text" id="user-name"
