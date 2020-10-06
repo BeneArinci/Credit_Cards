2843059261820289e17dd696619f095982f7cae8
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-10-06 08:22:04 +0100

    Styled InvalidInput component

--- client/src/components/InvalidInput/InvalidInput.js
+++ client/src/components/InvalidInput/InvalidInput.js
@@ -1,9 +1,10 @@
 import React from "react";
 import BackLink from "../BackLink/BackLink"
+import './InvalidInput.css'
 
 const InvalidInput = () => {
   return (
-    <div>
+    <div className = "page-container bg-washed-green shadow-3 center">
       <h1>Something went wrong..</h1>
       <h3>The data you entered is not valid<br />Please, go back and enter again your data</h3>
       <BackLink />
